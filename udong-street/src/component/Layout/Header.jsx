09742d0456c6c819edc387a43c34eaf5ff0895bf
omj9803 <myeongju0316@gmail.com>
2022-03-10 18:03:19 +0900

    :lipstick: header component style 적용

--- udong-street/src/component/Layout/Header.jsx
+++ udong-street/src/component/Layout/Header.jsx
@@ -1,8 +1,17 @@
+import './Header.css';
+import CurrentDate from './CurrentDate';
+import CurrentLocation from './CurrentLocation';
+
 const Header = () => {
     return (
-        <div>
-            Header
+        <div id='header'>
+            <CurrentDate />
+            <CurrentLocation />
+            <div id='title'>
+                우<a>리</a>동<a>네</a>거리<a>두기</a>
+            </div>
         </div>
+
     )
 }
 export default Header
