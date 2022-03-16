23b0a9f9d639a820dbc35a5a185f0e06f22d3a7e
omj9803 <myeongju0316@gmail.com>
2022-03-16 17:15:51 +0900

    :page_facing_up: Add Source

---
+++ udong-street/src/component/Layout/Source.jsx
@@ -0,0 +1,9 @@
+
+const Source = () => {
+    return (
+        <div className="source" onClick={window.open('http://www.mohw.go.kr/react/al/sal0301ls.jsp?PAR_MENU_ID=04&MENU_ID=0403&BOARD_ID=140&SEARCHKEY=DEPT_NM&SEARCHVALUE=%EC%BD%94%EB%A1%9C%EB%82%98%EB%B0%94%EC%9D%B4%EB%9F%AC%EC%8A%A4&page=1')}>
+            출처 : 보건복지부 보도자료
+        </div>
+    )
+}
+export default Source
