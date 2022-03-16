def37d06b18be448ecaaa176e80450c22304b1fd
omj9803 <myeongju0316@gmail.com>
2022-03-16 17:12:33 +0900

    :lipstick: Header Layout 수정

--- udong-street/src/component/Layout/Header.jsx
+++ udong-street/src/component/Layout/Header.jsx
@@ -1,12 +1,12 @@
 import './Header.css';
 import CurrentDate from './CurrentDate';
-import CurrentLocation from './CurrentLocation';
+import Source from './Source';
 
 const Header = () => {
     return (
         <div id='header'>
             <CurrentDate />
-            <CurrentLocation />
+            <Source />
             <div id='title'>
                 우<a>리</a>동<a>네</a>거리<a>두기</a>
             </div>
