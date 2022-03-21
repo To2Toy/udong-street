f2eb317b99de353fb72bb2bb754205661b69da47
201702083 <201702083@o.cnu.ac.kr>
2022-03-21 19:02:18 +0900

    rename

---
+++ udong-street/src/component/Layout/index.jsx
@@ -0,0 +1,16 @@
+import Header from './Header'
+import Footer from './Footer'
+import Content from './Content'
+const Layout = () => {
+    return (
+        <div>
+            <Header/>
+            <Content/>
+            <Footer/>
+        </div>
+        
+        
+        
+    )
+}
+export default Layout
