de67bf2bafd45e57a734ab01034142c8fce7b672
omj9803 <myeongju0316@gmail.com>
2022-03-31 20:51:14 +0900

    :art: Add Tab-menu

--- udong-street/src/component/Layout/Content.jsx
+++ udong-street/src/component/Layout/Content.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react"
+import { Nav } from "react-bootstrap"
+import Tab1 from './Tab1_Rule';
+import Tab2 from './Tab2_Map'
+
+function TabContent(props) {
+    if (props.tab === 0) {
+        return <Tab1></Tab1>
+    }
+    else if (props.tab === 1) {
+        return <Tab2></Tab2>
+    }
+}
+
 const Content = () => {
+    let [tab, setTab] = useState(0)
     return (
-        <div>
-            Content
+        <div id="menu-bar">
+            <Nav fill variant="tabs" defaultActiveKey="link-0">
+                <Nav.Item>
+                    <Nav.Link eventKey="link-0" onClick={() => { setTab(0) }}>
+                        우리 동네 규칙
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1" onClick={() => { setTab(1) }}>
+                        전국 확진 추이
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <TabContent tab={tab}/>
         </div>
     )
 }
+
+
 export default Content
