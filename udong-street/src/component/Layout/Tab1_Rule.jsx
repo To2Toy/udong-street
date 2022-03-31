ce87c1a603e092c89b0b79b9a25ee96e4f901d43
omj9803 <myeongju0316@gmail.com>
2022-03-31 20:54:20 +0900

    :lipstick: New Tab-menu component

---
+++ udong-street/src/component/Layout/Tab1_Rule.jsx
@@ -0,0 +1,10 @@
+
+const Tab1_Rule = () => {
+    return (
+        <div>
+            우리 동네 규칙을 알아봅시다!
+        </div>
+
+    )
+}
+export default Tab1_Rule
---
+++ udong-street/src/component/Layout/Tab2_Map.jsx
@@ -0,0 +1,10 @@
+
+const Tab2_Map = () => {
+    return (
+        <div>
+            전국 확진 추이를 알아봅시다!
+        </div>
+
+    )
+}
+export default Tab2_Map
