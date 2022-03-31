de9ee000541f07443d7dc3b0b0ae96653582c1f0
201702083 <201702083@o.cnu.ac.kr>
2022-03-31 21:30:22 +0900

    :fire: Layout css 통합

--- udong-street/src/component/Layout/Header.jsx
+++ udong-street/src/component/Layout/Header.jsx
@@ -1,13 +1,13 @@
-import './Header.css';
+import './index.css';
 import CurrentDate from './CurrentDate';
 import Source from './Source';
 
 const Header = () => {
     return (
-        <div id='header'>
+        <div className='header'>
             <CurrentDate />
             <Source />
-            <div id='title'>
+            <div className='title'>
                 우<a>리</a>동<a>네</a>거리<a>두기</a>
             </div>
         </div>
