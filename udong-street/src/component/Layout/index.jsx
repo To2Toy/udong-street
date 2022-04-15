93acc83d6ace474554af5bc3019112352afbfc7b
201702083 <201702083@o.cnu.ac.kr>
2022-04-15 15:43:54 +0900

    :lipstick: Fix footer bottom always

--- udong-street/src/component/Layout/index.jsx
+++ udong-street/src/component/Layout/index.jsx
@@ -1,17 +1,16 @@
-import Header from './Header'
-import Footer from './Footer'
-import Content from './Content'
-import './index.css'
+import Header from "./Header";
+import Footer from "./Footer";
+import Content from "./Content";
+import "./index.css";
 const Layout = () => {
-    return (
-        <div>
-            <Header/>
-            <Content/>
-            <Footer/>
-        </div>
-        
-        
-        
-    )
-}
-export default Layout
+  return (
+    <div id="_wrapper">
+      <div id="wrapper">
+        <Header />
+        <Content />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+export default Layout;
